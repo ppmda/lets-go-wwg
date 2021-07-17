d3a9d920a8106d64466029e8d03559df0363deba
Pamela Silva <pmlclh@gmail.com>
2021-07-17 14:53:18 -0300

    agora vai!!!!

--- main.go
+++ main.go
@@ -3,16 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	cores := map[string]string{
-		"cinza": "#424242",
-		"roxo":  "#a378f8",
+	hora := 35
+
+	switch {
+	case hora >= 6 && hora < 12:
+		fmt.Printf("Agora são %d horas e é de manhã", hora)
+
+	case hora >= 12 && hora < 18:
+		fmt.Printf("Agora são %d horas e é de tarde", hora)
+
+	case hora >= 18 && hora < 24:
+		fmt.Printf("Agora são %d horas e é de noite", hora)
+
+	case hora >= 0 && hora < 6:
+		fmt.Printf("Agora são %d horas e é de madruga", hora)
+	default:
+		fmt.Printf("O valor %d informado não é uma hora válida", hora)
 	}
-	fmt.Println("1", cores)
-
-	delete(cores, "cinza")
-	fmt.Println("2", cores)
-
-	//azul, existe := cores["azul"]
-	//fmt.Println(azul)
-	//fmt.Println(existe)
 }
